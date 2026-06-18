0b228f8aa223da931a47dbf9bee8654362bb33c9
Dominik Tkacik <tkacik38@gmail.com>
2026-06-18 12:40:27 +0200

    request-validation

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,9 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.1.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [0.9.0] - 2026-06-18
+ - Added support for `request-validation` plugin.
+
 ## [0.8.2] - 2026-06-15
 
 ## Changed
--- src/apisix_client/plugin/models/__init__.py
+++ src/apisix_client/plugin/models/__init__.py
@@ -2,5 +2,5 @@ from apisix_client.plugin.models.authentication import BaseAuth, KeyAuth, KeyAut
 from apisix_client.plugin.models.base import Plugins
 from apisix_client.plugin.models.observability.loggers import ClickhouseLogger, FileLogger
 from apisix_client.plugin.models.security import ConsumerRestriction
-from apisix_client.plugin.models.traffic import LimitCount
+from apisix_client.plugin.models.traffic import LimitCount, RequestValidation, RequestValidationSchema, RequestPropertySchema
 from apisix_client.plugin.models.transformation import ProxyRewrite, ResponseFilter, ResponseRewrite
